47d8444479509d97b66a3fa77f279b9ab921878f
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2017-05-19 22:02:04 +0100

    Initial project setup

---
+++ desktop/src/tv/rocketbeans/supermafiosi/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package tv.rocketbeans.supermafiosi.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import tv.rocketbeans.supermafiosi.SuperMafiosiGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SuperMafiosiGame(), config);
+	}
+}
