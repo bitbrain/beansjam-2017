2450729595fc07fa2bb380ef43fdf1a263242db2
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2017-05-21 00:00:54 +0100

    Fix the build. whoops

--- core/src/tv/rocketbeans/supermafiosi/minigame/roulette/RouletteMiniGame.java
+++ core/src/tv/rocketbeans/supermafiosi/minigame/roulette/RouletteMiniGame.java
@@ -1,5 +1,6 @@
 package tv.rocketbeans.supermafiosi.minigame.roulette;
 
+import tv.rocketbeans.supermafiosi.core.MafiosiGameContext;
 import tv.rocketbeans.supermafiosi.minigame.AbstractMiniGame;
 
 /**
@@ -7,10 +8,15 @@ import tv.rocketbeans.supermafiosi.minigame.AbstractMiniGame;
  * robin. The chances are higher for a shot if more bullets are loaded.
  */
 public class RouletteMiniGame extends AbstractMiniGame {
+	
+	private final MafiosiGameContext context;
+	
+	public RouletteMiniGame(MafiosiGameContext context) {
+		this.context = context;
+	}
 
 	@Override
 	public void initialise() {
-		// XXX Auto-generated method stub
 		
 	}
 
