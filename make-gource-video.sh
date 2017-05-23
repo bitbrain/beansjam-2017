e12f1bcb41783fb1d75b8da89c99958eac96a563
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2017-05-23 21:56:48 +0100

    Add gource video sh

---
+++ make-gource-video.sh
@@ -0,0 +1,10 @@
+gource \
+    -1920x1080 \
+    --multi-sampling \
+    --stop-at-end \
+    --key \
+    --highlight-users \
+    --background-colour 000000 \
+    --font-size 22 \
+    --title "Super Don #beansjam development (48 hours)" \
+    --output-framerate 60 \
